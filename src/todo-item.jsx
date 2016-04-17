5ce397406e15a17e726c0b4c56a4d5a20bab5bd0
Corey Ferguson <corey.t.ferguson@gmail.com>
2016-04-17 10:44:23 -0700

    TodoItem state text

--- src/todo-item.jsx
+++ src/todo-item.jsx
@@ -4,14 +4,25 @@ import React from 'react';
 
 export default class TodoItem extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: this.props.children.text
+    }
+  }
+
   render() {
     return (
       <li>
         <input type='checkbox' />
-        <input type='text' value={this.props.children.text} />
+        <input type='text' value={this.state.text} onChange={this.change.bind(this)} />
         <button type='button' onClick={this.delete.bind(this)}>Delete</button>
       </li>
     );
+  }
+
+  change(event) {
+    this.setState({text: event.target.value});
   }
 
   delete() {
